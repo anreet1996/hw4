14936218273d9d515e1f8e086f0cd6277519d9a8
anreet1996 <90850863+anreet1996@users.noreply.github.com>
2026-06-09 04:29:08 +0000

    Require login to access places and entries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  def require_login
+    if @current_user.nil?
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :require_login
+
   def index
     @places = Place.all
   end
